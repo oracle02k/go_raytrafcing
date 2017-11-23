04adb6866fb64460f62672381abcf4f99b00fed5
n_akira <oracle02k@gmail.com>
2017-11-23 21:56:23 +0900

    カメラ制御を追加

--- util3d/camera.go
+++ util3d/camera.go
@@ -12,16 +12,20 @@ type Camera struct {
 	vertical        math3d.Vec3
 }
 
-func NewCamera(vfov, aspect float64) *Camera {
+func NewCamera(from, at, vup math3d.Vec3, vfov, aspect float64) *Camera {
 	theta := vfov * math.Pi / 180
 	halfHeight := math.Tan(theta / 2)
 	halfWidth := aspect * halfHeight
 
+	w := math3d.MakeUnitVector(from.Sub(at))
+	u := math3d.MakeUnitVector(math3d.Vec3Closs(vup, w))
+	v := math3d.Vec3Closs(w, u)
+
 	return &Camera{
-		origin:          math3d.NewVec3(0.0, 0.0, 0.0),
-		lowerLeftCorner: math3d.NewVec3(-halfWidth, -halfHeight, -1.0),
-		horizontal:      math3d.NewVec3(2*halfWidth, 0.0, 0.0),
-		vertical:        math3d.NewVec3(0.0, 2*halfHeight, 0.0),
+		origin:          from,
+		lowerLeftCorner: from.Sub(u.Scale(halfWidth)).Sub(v.Scale(halfHeight)).Sub(w),
+		horizontal:      u.Scale(2 * halfWidth),
+		vertical:        v.Scale(2 * halfHeight),
 	}
 }
 
